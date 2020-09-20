1eb3543f9920154c1332ca057687f5ec0972abcd
Lars Behrenberg <l.behrenberg@gmail.com>
2020-09-20 18:39:16 +0900

    Fix about page query

--- src/templates/page.js
+++ src/templates/page.js
@@ -7,28 +7,24 @@ import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
 
 /**
-* Single page (/:slug)
-*
-* This file renders a single page and loads all the content.
-*
-*/
+ * Single page (/:slug)
+ *
+ * This file renders a single page and loads all the content.
+ *
+ */
 const Page = ({ data, location }) => {
     const page = data.ghostPage
 
     return (
         <>
-            <MetaData
-                data={data}
-                location={location}
-                type="website"
-            />
+            <MetaData data={data} location={location} type="website" />
             <Helmet>
                 <style type="text/css">{`${page.codeinjection_styles}`}</style>
             </Helmet>
-            <Layout>
+            <Layout content={page}>
                 <div className="container">
                     <article className="content">
-                        <h1 className="content-title">{page.title}</h1>
+                        {/* <h1 className="content-title">{page.title}</h1> */}
 
                         {/* The main page content */}
                         <section
